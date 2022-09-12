1de0775f5bc5d54d81184993ba0e0b13c05157b7
nnodubukkie <nnodubukkie@gmail.com>
2022-09-12 09:17:10 -0700

    print alphabet commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main = print alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++);
+	{
+		putchar(alp[i]);
+	}
+	putchar ('\n');
+	return (0);
+}
