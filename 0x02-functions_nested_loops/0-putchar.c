7a06f413c9088d319fe18f4fbabbdd959be46eb2
nnodubukkie <nnodubukkie@gmail.com>
2022-09-14 13:13:14 -0700

    print _putchar commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,17 +9,16 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	putchar('_');  
 	putchar('p');
 	putchar('u');
 	putchar('t');
 	putchar('c');
-	putchar('h');
-        putchar('a');
-       	putchar('r');
-	putchar('\n');
+	putchar('h');                                                                                     
+	putchar('a');                                                                                     
+	putchar('r');
+ 	putchar('\n');
 
-	return (0);
+	return 	(0);
 }
