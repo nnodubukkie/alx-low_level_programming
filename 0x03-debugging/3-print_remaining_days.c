7319419b55ca2f80ab65e60dd74132ec9e958e41
nnodubukkie <nnodubukkie@gmail.com>
2022-09-15 07:40:08 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,11 +19,11 @@ day++;
 }
 
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 -day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month == 2 && day ==60)
+if (month == 2 && day == 60)
 {
 printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
