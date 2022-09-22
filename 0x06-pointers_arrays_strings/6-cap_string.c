58f398c5f1ba686cf630249cc379406ace7b6271
nnodubukkie <nnodubukkie@gmail.com>
2022-09-21 17:13:58 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,34 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: returns length as integer;
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
  * cap_string - capitalizes all words of a string.
  * @str: The string to be capitalized.
- *
  * Return: A pointer to the changed string.
  */
+
 char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[index])
+	while (str[++index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		while (!(str[index] >= 'a') && str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
@@ -30,8 +46,6 @@ char *cap_string(char *str)
 		    str[index - 1] == '}' ||
 		    index == 0)
 			str[index] -= 32;
-
-		index++;
 	}
 
 	return (str);
