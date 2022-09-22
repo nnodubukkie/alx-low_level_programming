26908f10c7046f69fd1b936358ebdb3587f2d901
nnodubukkie <nnodubukkie@gmail.com>
2022-09-22 16:04:27 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,24 @@
-#include "main.h"
+#include "main.h"                                                                
 
-/**
- * _strncpy - copies at most an inputted number
- * 	of bytes from string src into dest.
- * @dest: the buffer storing the string copy
- * @src: The source string
- * @n: The maximum number of bytes to copied from src
- *
- * Returns: & pointer to the resulting string dest.
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int index = 0, src_len = 0;
-
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
-	return (dest);
-}
+/**                                                                              
+ * _strncpy - C function that copies a string, including the                     
+ * terminating null byte, using at most an inputted number of bytes.             
+ * if the length of the source string is less than the maximum bytes number.     
+ * the remainder of the destination string is filled with null bytes.            
+ * works identically to the standard library function 'strncpy',                 
+ * @dest: Buffer storing the string copy                                         
+ * @src: the source string                                                       
+ * @n: max number of byte copied                                                 
+ * Return: returns                                                               
+ */                                                                              
+                                                                             
+char *_strncpy(char *dest, char *src, int n)                                     
+{		
+	int i;                                                                        	
+	for (i = 0; i < n && src[i] != '\0'; i++                                
+		dest[i] = src[i];                                                                                    
+        for ( ; i < n; i++)                                                      
+                dest[i] = '\0';                                                  
+                                                                                
+        return (dest);                                                           
+}                                        
