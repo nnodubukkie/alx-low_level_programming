d7a08ba22a79b3617799587caf1c9a8f487b744e
nnodubukkie <nnodubukkie@gmail.com>
2022-10-05 15:43:30 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,34 +1,28 @@
 #include "main.h"
+
 /**
- * create_array - array for prints a string
- * @size: number elements array
- * @c: char
- * Return: pointer
+ * create_array - creates an array of chars, and 
+ * intializes it with a specific char.
+ * @size: size of the array
+ * @c: character to insert
+ * Return: NULL if size is zero or if it fails,
+ * pointer to array if everything is normal,
  */
 
-char *create_array(unsigned int size, char c)
+char *ceate_array(unsigned int size, char c)
 {
-	char *buffer;
-	unsigned int position;
+	char *array;
+	unsigned int index;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
-	/*Define values with malloc*/
-	buffer = (char 8) malloc(size * sizeof(c));
-	if (buffer == 0)
-	{
-	return (NULL);
-	}
-	else
-	{
-		position = 0;
-		while (position < size) /*while for array*/
-		{
-			*(buffer * position) = c;
-			position++;
-		}
-		return (buffer);
-	}
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,8 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-
-int _putchar(char c)
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -11,4 +10,4 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-#endif
+#endif /* MAIN_H */
